e5e3d2067a9c186391a5530ba06b177e772f273f
Md. Ashraful A. <aaj.cse@gmail.com>
2014-11-06 21:13:51 +0600

    Create jqpack.jquery.js

---
+++ jqpack.jquery.js
@@ -0,0 +1,28 @@
+jQuery( document ).ready(function() {
+        jQuery('.attribute-nav ol').each(function(){
+            var ol = this;
+            var mr = jQuery('li:gt(4)', this).length;
+            var me = jQuery('.show-links', this).length;
+            if(mr && !me){
+                jQuery('li:gt(4)', this).hide();
+
+                jQuery(this).append('<li class="show-links show-more"> More</li>');
+                jQuery(this).append('<li class="show-links show-less"> Less</li>');
+                jQuery('.show-less',this).hide();
+
+                jQuery('.show-more', this).bind('click', function(){
+                    jQuery('li:gt(4)', ol).show();
+                    jQuery('.show-more', ol).hide();
+                    jQuery('.show-less', ol).show();
+                });
+
+                jQuery('.show-less', this).bind('click', function(){
+                    jQuery('li:gt(4)', ol).hide();
+                    jQuery('.show-more', ol).show();
+                    jQuery('.show-less', ol).hide();
+                });
+            }
+
+        });
+
+    });
